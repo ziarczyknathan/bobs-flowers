5011880f57d43d54a345185514639a8f483b142e
Nathan Ziarczyk <nathan.ziar@gmail.com>
2022-04-07 22:58:16 +0200

    added dynamic product option selectors

--- assets/app.js
+++ assets/app.js
@@ -58,4 +58,54 @@
       handleUrlChanged();
     });
   }
+
+  // Add to cart
+  const $productOptionSelectors = document.querySelectorAll(
+    ".product-option-selector"
+  );
+  const $selectedIdField = document.querySelector("input[name=id]");
+  const $submitButton = document.querySelector("button[type=submit]");
+
+  const disableAddToCart = () => {
+    $submitButton.disabled = true;
+    $submitButton.classList.add("disabled");
+    $selectedIdField.value = "";
+  };
+
+  const equals = (a, b) =>
+    a.length === b.length && a.every((v, i) => v === b[i]);
+
+  const getAllOptionValues = () => {
+    const values = [];
+    $productOptionSelectors.forEach((selector) => {
+      values.push(selector.value);
+    });
+    return values;
+  };
+
+  if (window.product && $productOptionSelectors) {
+    const variants = window.product.variants;
+
+    $productOptionSelectors.forEach((selector) => {
+      selector.addEventListener("change", (e) => {
+        const values = getAllOptionValues();
+        const found = variants.find((variant) => {
+          return equals(variant.options, values);
+        });
+
+        if (found) {
+          var newurl =
+            window.location.protocol +
+            "//" +
+            window.location.host +
+            window.location.pathname +
+            "?variant=" +
+            found.id;
+          window.location.replace(newurl);
+        } else {
+          disableAddToCart();
+        }
+      });
+    });
+  }
 })();
