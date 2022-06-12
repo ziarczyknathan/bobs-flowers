e31e0644043bce45c905d39b997929c2c9173a12
Nathan Ziarczyk <nathan.ziar@gmail.com>
2022-06-12 22:38:11 +0200

    feat: edit vat field

--- js/modules/loginRegister.js
+++ js/modules/loginRegister.js
@@ -1,4 +1,4 @@
-import { checkVAT, belgium, austria } from "jsvat";
+import { checkVAT, belgium } from "jsvat";
 export const showHideRecoverForm = () => {
   const loginForm = document.querySelector(".form__login");
   const recoverForm = document.querySelector(".form__recover");
@@ -35,20 +35,71 @@ export const validateVat = () => {
   });
 };
 
+function disable_form(form) {
+  var inputs = form.getElementsByTagName("input"),
+    textareas = form.getElementsByTagName("textarea"),
+    buttons = form.getElementsByTagName("button"),
+    selects = form.getElementsByTagName("select");
+
+  disable_elements(inputs);
+  disable_elements(textareas);
+  disable_elements(buttons);
+  disable_elements(selects);
+}
+function disable_elements(elements) {
+  var length = elements.length;
+  while (length--) {
+    elements[length].disabled = true;
+  }
+}
+
 export const toggleVatForm = () => {
   const vatNumberDisplay = document.querySelector(".vat-number-display");
   const vatNumberForm = document.querySelector(".vat-number-form");
-
-  if (!vatNumberDisplay || !vatNumberForm) return;
+  const vatInput = document.querySelector(
+    ".vat-number-form input[id=vat_reg_no]"
+  );
+  const companyName = document.querySelector(
+    ".vat-number-form input[id=comp_name]"
+  );
+  const customerId = document.querySelector("input#customer_id")?.value;
 
   const vatEditButton = document.querySelector(
     ".vat-number-display .vat-edit-button"
   );
 
+  if (!vatNumberDisplay || !vatNumberForm || !vatEditButton) return;
+
   vatEditButton.addEventListener("click", (e) => {
-    console.log(e);
     vatNumberDisplay.classList.add("d-none");
     vatNumberForm.classList.remove("d-none");
+  });
+
+  vatNumberForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+    const vatNrKey = vatInput.attributes.name.value;
+    const vatNrValue = vatInput.value;
+    const companyNameKey = companyName.attributes.name.value;
+    const companyNameValue = companyName.value;
+
+    const body = {
+      metafields: {
+        [vatNrKey]: vatNrValue,
+        [companyNameKey]: companyNameValue,
+      },
+      customer_id: customerId,
+    };
+    disable_form(vatNumberForm);
+    fetch("/apps/customer_notes_to_metafields/update-preferences/", {
+      method: "POST",
+      body: JSON.stringify(body),
+      headers: {
+        "Cache-Control": "max-age=0",
+        "Content-Type": "application/json",
+      },
+    })
+      .then((res) => res.json())
+      .finally(() => window.location.reload());
   });
 };
 
